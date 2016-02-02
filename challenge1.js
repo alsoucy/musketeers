3fac6e84d1a9a3daadf8cb581ffc5741c2dfe38d
Aimee Soucy <alsoucy74@gmail.com>
2016-02-01 19:44:46 -0800

    commit

--- challenge1.js
+++ challenge1.js
@@ -18,12 +18,25 @@ function rn(min, max) {
 for(var i = 0; randomNums.length < 30; i++){
   randomNums.push(rn(1, 1000))
 };
+console.log(randomNums);
 
 for(var i = 0; randomNums.length < 30; i++) {
   console.log(randomNums[i] + '|');
 };
 
 //when I run above in the console and then call the randomNums array it brings back the same array over and over instead of bringing a new array every time. What am I doing wrong?
-Math.min.apply(Math.(parseInt(randomNums[])));
+Math.min(parseInt(randomNums));
 Math.max(parseInt(randomNums));
 //when I run the above in the console, both bring up first number in the array
+var min = Math.min(...randomNums);
+min;
+//above works, but I'm not sure why this works and Math.min(parseInt(randomNums)) doesn't; what does '...' do?
+
+// to iterate through the array to find smallest number;
+var min = randomNums[0];
+for(var i = 0; i < randomNums.length; i++) {
+  if( randomNums[i] < min) {
+    min = randomNums[i];
+  }
+}
+console.log(min);
