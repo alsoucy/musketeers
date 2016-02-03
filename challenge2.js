10e5bb85d91952003d11e806ef00497011a13af0
Aimee Soucy <alsoucy74@gmail.com>
2016-02-02 20:18:41 -0800

    initial commit

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+// Given a string and a number n, return a new string where the char at index n has been removed.
+// examples:
+// missingChar("kitten", 1) → "ktten"
+// missingChar("kitten", 0) → "itten"
+// missingChar("kitten", 4) → "kittn"
+// instead of returning, you can print it
+// I suggest to start discussing the pseudo-code for it
+// the string can either be hard-coded or you can ask the user for it, whichever you prefer
+// same for the number
