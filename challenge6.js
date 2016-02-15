67ea37523095d830921520dfd570fdc84ff16a15
Aimee Soucy <alsoucy74@gmail.com>
2016-02-15 02:32:39 -0800

    ch 6 solutions

--- challenge6.js
+++ challenge6.js
@@ -9,14 +9,42 @@
 // and 55 is calculated based on the last two positions (21+34)
 // fibo(n) = fibo(n - 1) + fibo(n - 2)
 
-function fibo(11) = 55
-fibo[0] = 0
-fibo[1] = 1
-fibo[2] = 1
-fibo[3] = 2
-fibo[4] = 3
-fibo[5] = 5
-fibo[6] = 8
-fibo[7] = 13
-fibo[8] = 21
-fibo[9] = 34
+// function fibo(11) = 55
+// fibo[0] = 0
+// fibo[1] = 1
+// fibo[2] = 1
+// fibo[3] = 2
+// fibo[4] = 3
+// fibo[5] = 5
+// fibo[6] = 8
+// fibo[7] = 13
+// fibo[8] = 21
+// fibo[9] = 34
+// fibro[10] = 55
+// fibro[11] = 89
+// fibro[12] = 144
+
+  function fib1(n){
+  var x = 0;
+  var y = 1;
+  if(n <= 2){
+    return n-1;
+  }
+  for( var i = 0; i < n; i++){
+    var tempY = y;
+    y = tempY + x;
+    x = tempY;
+    }
+  return y;
+}
+console.log(fib1(10));
+
+function fib(n) {
+  var result = 0;
+  if (n <= 2){
+    return n-1;
+  }
+  result = fib(n-1) + fib(n-2);
+  return result;
+};
+console.log(fib(7));
